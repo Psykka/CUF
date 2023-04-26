9acf4942cfb93a07ac8d4207451690dbf750eabb
Psykka <36780964+Psykka@users.noreply.github.com>
2023-04-26 11:37:42 -0300

    first commit

---
+++ CUF/Controllers/LandController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CUF.Models;
+
+namespace CUF.Controllers;
+
+public class LandController : Controller
+{
+    private readonly ILogger<LandController> _logger;
+
+    public LandController(ILogger<LandController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
